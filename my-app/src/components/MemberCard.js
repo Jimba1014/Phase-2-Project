360610b45c0d5397eee6bf85ffccb9b8c0f8091d
Jim <jimba.ed@outlook.com>
2022-10-03 18:00:15 -0400

    added hidden text to div

--- my-app/src/components/MemberCard.js
+++ my-app/src/components/MemberCard.js
@@ -1,17 +1,28 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import { Link } from 'react-router-dom'
 
 function MemberCard({ id, name, instruments, genre, artists, song, memory, link }) {
+
+    const [hidden, setHidden] = useState(false)
+
+    function handleClick(){
+        setHidden(prev => !prev)
+    }
+
+    
     return(
-        <div>
+        <div onClick = {handleClick}>
             <h1>{name}</h1>
             <Link to={link}>{link}</Link>
             <div className='hidden-component'>
-                <p>Instruments that I can Play: {instruments}</p>
-                <p>My Favorite Genre of Music: {genre} </p>
-                <p>My Favorite Artists: {artists} </p>
-                <p>The song that I've benn listening to on repeat: {song}</p>
-                <p>My favorite musical memory: {memory}</p>
+                {hidden ?
+                <div>
+                    <p>Instruments that I can Play: {instruments}</p>
+                    <p>My Favorite Genre of Music: {genre} </p>
+                    <p>My Favorite Artists: {artists} </p>
+                    <p>The song that I've benn listening to on repeat: {song}</p>
+                    <p>My favorite musical memory: {memory}</p>
+                </div> : null }
             </div>
         </div>
     )
