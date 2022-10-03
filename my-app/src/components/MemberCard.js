45ae3c40da3109d06683c9f51bf825121fc5d37c
Jim <jimba.ed@outlook.com>
2022-10-03 14:30:37 -0400

    added content to the member card

--- my-app/src/components/MemberCard.js
+++ my-app/src/components/MemberCard.js
@@ -3,7 +3,14 @@ import React from 'react';
 function MemberCard() {
     return(
         <div>
-            Member Card
+            <h1>Name</h1>
+            <div>Spotify Link: </div>
+            <div className='hidden-component'>
+                <p>Instrument that I can Play:</p>
+                <p>My Favorite Genre of Music:</p>
+                <p>The song that I've benn listening to on repeat:</p>
+                <p>My favorite musical memory</p>
+            </div>
         </div>
     )
 }
