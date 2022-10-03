2fbecfd77a01ee65c8c0f71b69f196ae8c9570f4
Briana <briana_brewster4@yahoo.com>
2022-10-03 14:27:34 -0500

    updated Form component

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,9 +1,18 @@
 import React from 'react'; 
 
 function Form() {
+
+    
     return(
-        <div>
-            Form
+        <div className="new-member-form">
+            <h2>Add New Member</h2>
+            <input type="text" name="name" placeholder="Your name" />
+            <input type="text" name="instruments" placeholder="Instruments you play" />
+            <input type="text" name="genre" placeholder="Genres you listen to" />
+            <input type="text" name="artists" placeholder="Your favorite artists" />
+            <input type="text" name="song" placeholder="Your favorite song" />
+            <input type="text" name="memory" placeholder="Your favorite music memory" />
+            <input type="text" name="link" placeholder="Spotify profile link" />
         </div>
     )
 }
