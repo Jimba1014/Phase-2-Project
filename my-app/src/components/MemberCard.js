ae162122fdd3adabe73ccfdfcfcb7699869bd2dd
Briana <briana_brewster4@yahoo.com>
2022-10-03 14:30:11 -0500

    updated MemberCard component

--- my-app/src/components/MemberCard.js
+++ my-app/src/components/MemberCard.js
@@ -3,13 +3,13 @@ import React from 'react';
 function MemberCard({ id, name, instruments, genre, artists, song, memory, link }) {
     return(
         <div>
-            <h1>Name</h1>
-            <div>Spotify Link: </div>
+            <h1>{name}</h1>
+            <div>{link}</div>
             <div className='hidden-component'>
-                <p>Instrument that I can Play:</p>
-                <p>My Favorite Genre of Music:</p>
-                <p>The song that I've benn listening to on repeat:</p>
-                <p>My favorite musical memory</p>
+                <p>Instruments that I can Play: {instruments}</p>
+                <p>My Favorite Genre of Music: {genre} </p>
+                <p>The song that I've benn listening to on repeat: {song}</p>
+                <p>My favorite musical memory: {memory}</p>
             </div>
         </div>
     )
