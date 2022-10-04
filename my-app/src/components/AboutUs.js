6b2120303a9b3f8dd0b68d3ed1052fa9c25cb4bb
Briana <briana_brewster4@yahoo.com>
2022-10-04 15:34:17 -0500

    added AboutUs text

--- my-app/src/components/AboutUs.js
+++ my-app/src/components/AboutUs.js
@@ -5,8 +5,21 @@ function AboutUs() {
         <div className='about'>
            <h2>About The Creators</h2>
            <div>
-            <img src="https://cvws.icloud-content.com/S/ATJCUqMvETyq3QcOFB-MvjiJZzqG/IMG_9253.jpg?o=AkaK-QNA85x5mEJv-tfAqxUrZjHtGGJyIF2y2TxNPdUy&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogTsc64GRJyiSpOaWUK1K7pJTrKum7mo3U1lsr5Q1VfAcSZxCnivejujAYp6GKqbowIgEAUgSJZzqGaiZ16iKaUj5tZ10i3x__7uIgzCUsRnN0sGtrs_I0Z1eaGkDS_mDP9XImmeqeMXTfd1LAQWillbuBM_zyw8c8OUGoJrXJMDYNuQvuIm2hVYI&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-4&s=MirGp3fwxKLLevqAAbKUFbHg2bM" alt="Jim Baldridge"/>
-            <img src="https://cvws.icloud-content.com/S/AfEJn-YwGI-E59qGpJKGqXGYBCVD/IMG_3982.JPG?o=AgM6OhdIfK2uwSt0Lfum4NIJHZVOI6hN6E_ZMCivLQP4&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogiPl2l3hFnf0oQlNhfKcU2RxCUYR1LV1HZKF_NN_LBjsSZxCnivejujAYp6GKqbowIgEAUgSYBCVDaibmUg3Z6ZJX-hVxKLAUA9LGz7dPIDcUR1tsUvpYyO24E-We8E7BMnImDj-cP2rHJ6zXusdqCtuvaaDR-b3emVKsKV0TWp0TwjBAFCCUg_0&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-2&s=38G5fOXsjs05mpDwmUGVQDfmSXE" alt="Briana Brewster"/>
+            <img 
+            src="https://cvws.icloud-content.com/S/ATJCUqMvETyq3QcOFB-MvjiJZzqG/IMG_9253.jpg?o=AkaK-QNA85x5mEJv-tfAqxUrZjHtGGJyIF2y2TxNPdUy&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogTsc64GRJyiSpOaWUK1K7pJTrKum7mo3U1lsr5Q1VfAcSZxCnivejujAYp6GKqbowIgEAUgSJZzqGaiZ16iKaUj5tZ10i3x__7uIgzCUsRnN0sGtrs_I0Z1eaGkDS_mDP9XImmeqeMXTfd1LAQWillbuBM_zyw8c8OUGoJrXJMDYNuQvuIm2hVYI&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-4&s=MirGp3fwxKLLevqAAbKUFbHg2bM" 
+            alt="Jim Baldridge"/>
+            <img 
+            src="https://cvws.icloud-content.com/S/AfEJn-YwGI-E59qGpJKGqXGYBCVD/IMG_3982.JPG?o=AgM6OhdIfK2uwSt0Lfum4NIJHZVOI6hN6E_ZMCivLQP4&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogiPl2l3hFnf0oQlNhfKcU2RxCUYR1LV1HZKF_NN_LBjsSZxCnivejujAYp6GKqbowIgEAUgSYBCVDaibmUg3Z6ZJX-hVxKLAUA9LGz7dPIDcUR1tsUvpYyO24E-We8E7BMnImDj-cP2rHJ6zXusdqCtuvaaDR-b3emVKsKV0TWp0TwjBAFCCUg_0&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-2&s=38G5fOXsjs05mpDwmUGVQDfmSXE" 
+            alt="Briana Brewster"/>
+           </div>
+
+           <div className='about-creators'>
+            <p>
+                Meet the creators of the app, Jim Baldridge and Briana Brewster!<br></br>
+                Briana's interest in music began when she joined her school's music program at 10 years old. She began playing the French Horn and later branched out to playing other instruments, including trumpet, piano, and guitar.<br></br> 
+                Jim learned to play the drums in his school's band program and currently teaches the drumline at his alma mater.<br></br>
+                Jim and Briana wanted to apply their interest in music toward creating a platform for members of their Flatiron cohort to share their own musical interests and backgrounds with each other!
+            </p>
            </div>
         </div>
     )
