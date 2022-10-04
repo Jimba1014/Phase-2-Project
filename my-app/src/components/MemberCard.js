235badbabb069cd2f1d63da757044b62dab33650
Jim <jimba.ed@outlook.com>
2022-10-04 09:24:52 -0400

    added back in toggle feature

--- my-app/src/components/MemberCard.js
+++ my-app/src/components/MemberCard.js
@@ -1,18 +1,29 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import { Link } from 'react-router-dom'
 
 function MemberCard({ id, name, instruments, genre, artists, song, memory, link }) {
+
+    const [isHidden, setIsHidden] = useState(false)
+
+    function handleClick(){
+        setIsHidden(prev => !prev)
+    }
+
+
+
     return(
        <li className="card">
         <div className="card__content">
-            <h1 className="card__title">{name}</h1>
+            <h1 className="card__title" onClick={handleClick}>{name}</h1>
             <Link to={link}>{link}</Link>
             <div className="card__text">
-                <p>Instruments that I can Play: {instruments}</p>
-                <p>My Favorite Genre of Music: {genre} </p>
-                <p>My Favorite Artists: {artists} </p>
-                <p>The song that I've benn listening to on repeat: {song}</p>
-                <p>My favorite musical memory: {memory}</p>
+                {isHidden ? <div>
+                    <p>Instruments that I can Play: {instruments}</p>
+                    <p>My Favorite Genre of Music: {genre} </p>
+                    <p>My Favorite Artists: {artists} </p>
+                    <p>The song that I've benn listening to on repeat: {song}</p>
+                    <p>My favorite musical memory: {memory}</p>
+                </div> : null}
             </div>
         </div>
         </li>  
