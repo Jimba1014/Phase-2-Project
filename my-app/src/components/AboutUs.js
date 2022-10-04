e6f2537d070382ff7573aa01eedb1de05492a458
Briana <briana_brewster4@yahoo.com>
2022-10-04 14:32:18 -0500

    added img tags for AboutUs.js

--- my-app/src/components/AboutUs.js
+++ my-app/src/components/AboutUs.js
@@ -4,6 +4,10 @@ function AboutUs() {
     return(
         <div className='about'>
            <h2>About the creators</h2>
+           <div>
+            <img src="" alt="Jim Baldridge"/>
+            <img src="" alt="Briana Brewster"/>
+           </div>
         </div>
     )
 }
