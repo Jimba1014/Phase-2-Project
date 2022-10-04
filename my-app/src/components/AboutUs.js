bd59b87e7951f3351960526fe461daba7a60569f
Briana <briana_brewster4@yahoo.com>
2022-10-04 14:37:42 -0500

    added photos to AboutUs.js

--- my-app/src/components/AboutUs.js
+++ my-app/src/components/AboutUs.js
@@ -3,10 +3,10 @@ import React from 'react';
 function AboutUs() {
     return(
         <div className='about'>
-           <h2>About the creators</h2>
+           <h2>About The Creators</h2>
            <div>
-            <img src="" alt="Jim Baldridge"/>
-            <img src="" alt="Briana Brewster"/>
+            <img src="https://cvws.icloud-content.com/S/ATJCUqMvETyq3QcOFB-MvjiJZzqG/IMG_9253.jpg?o=AkaK-QNA85x5mEJv-tfAqxUrZjHtGGJyIF2y2TxNPdUy&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogTsc64GRJyiSpOaWUK1K7pJTrKum7mo3U1lsr5Q1VfAcSZxCnivejujAYp6GKqbowIgEAUgSJZzqGaiZ16iKaUj5tZ10i3x__7uIgzCUsRnN0sGtrs_I0Z1eaGkDS_mDP9XImmeqeMXTfd1LAQWillbuBM_zyw8c8OUGoJrXJMDYNuQvuIm2hVYI&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-4&s=MirGp3fwxKLLevqAAbKUFbHg2bM" alt="Jim Baldridge"/>
+            <img src="https://cvws.icloud-content.com/S/AfEJn-YwGI-E59qGpJKGqXGYBCVD/IMG_3982.JPG?o=AgM6OhdIfK2uwSt0Lfum4NIJHZVOI6hN6E_ZMCivLQP4&v=1&z=https%3A%2F%2Fp24-content.icloud.com%3A443&x=1&a=CAogiPl2l3hFnf0oQlNhfKcU2RxCUYR1LV1HZKF_NN_LBjsSZxCnivejujAYp6GKqbowIgEAUgSYBCVDaibmUg3Z6ZJX-hVxKLAUA9LGz7dPIDcUR1tsUvpYyO24E-We8E7BMnImDj-cP2rHJ6zXusdqCtuvaaDR-b3emVKsKV0TWp0TwjBAFCCUg_0&e=1664922849&r=78bc6b74-a441-4ccd-b1e1-dd116cecce15-2&s=38G5fOXsjs05mpDwmUGVQDfmSXE" alt="Briana Brewster"/>
            </div>
         </div>
     )
